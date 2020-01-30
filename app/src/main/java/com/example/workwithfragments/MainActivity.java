ee383a3915b9aaadbee0669c213df82de3adbd7e
Virginia <anikeeva.vira@gmail.com>
2020-01-30 11:24:05 +0200

    static and dynamic fragments added

--- app/src/main/java/com/example/workwithfragments/MainActivity.java
+++ app/src/main/java/com/example/workwithfragments/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.workwithfragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragment();
     }
+
+    private void addFragment(){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        DynamicFragment dynamicFragment = new DynamicFragment();
+        ft.add(R.id.dynamic_fragment, dynamicFragment);
+        ft.commit();
+
+    }
+
 }
